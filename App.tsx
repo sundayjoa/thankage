b4dda6871f7345344a8fc08beae47f012c160ffa
sundayjoa <jeonjoo1004@gmail.com>
2025-04-30 15:50:48 +0900

    [ADD] 땡기지 클론 초기 프로젝트 설정정

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>시작!</Text>
+      <Text>start!</Text>
       <StatusBar style="auto" />
     </View>
   );
